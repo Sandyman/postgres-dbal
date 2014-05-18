01808dfe17af80697b6e89367064659a904f3e2d
Sander Huijsen <sander.huijsen@gmail.com>
2014-05-18 17:08:35 +0800

    Changed copyright message.

--- lib/PostgresDBAL/DatabaseConnection.php
+++ lib/PostgresDBAL/DatabaseConnection.php
@@ -1,6 +1,6 @@
 <?php
 
-// Copyright (c) 2013, Richard Chipper
+// Copyright (c) 2013 Richard Chipper, Sander Huijsen
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
