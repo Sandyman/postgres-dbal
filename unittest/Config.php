80f33966cf011d5e7d7ca2eec94befa19eb71a4d
Sander Huijsen <sander.huijsen@gmail.com>
2014-05-18 17:22:40 +0800

    Database credentials in config file.

---
+++ unittest/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Database connection configuration.
+ */
+define('DATABASE_NAME', 'your database name goes here');
+define('DATABASE_USER', 'your database user name goes here');
+define('DATABASE_PASSWORD', 'your database password goes here');
+define('DATABASE_HOST', 'your database host goes here');
+
+?>
